5773070174bc2040f40d9d7f91cd506b8041386d
Chris Rycroft <chr@seas.harvard.edu>
2018-02-24 09:59:56 -0500

    Additional comments and improvements to 2D FFT code.

--- 2b_fft/pfft_test.cc
+++ 2b_fft/pfft_test.cc
@@ -3,9 +3,9 @@
 int main() {
     poisson_fft pf(32);
 
-    pf.init_mms();
+    pf.init();
     pf.solve();
 
-    pf.output("pfft.sol",true);
-    pf.output("pfft.inp",false);
+    pf.output_solution("pfft.sol");
+    pf.output_source("pfft.src");
 }
--- 2b_fft/poisson_fft.hh
+++ 2b_fft/poisson_fft.hh
@@ -1,6 +1,7 @@
 #ifndef POISSON_FFT
 #define POISSON_FFT
 
+#include "file_output.hh"
 #include <fftw3.h>
 
 class poisson_fft {
@@ -11,20 +12,34 @@ class poisson_fft {
         const int nn;
         /** The grid spacing. */
         const double h;
+        /** The discretized source term in the Poisson equation. */
         double* const f;
+        /** The discretized solution to the Poisson equation. */
         double* const v;
+        /** The frequency domain. */
         double* const w;
         poisson_fft(int n_);
         ~poisson_fft();
         void init();
         void solve();
-        void print();
         void init_mms();
         double l2_error_mms();
-        void output(const char* filename,bool solution);
+        void print(bool solution);
+        void output_solution(const char* filename);
+        /** Outputs the source term in the 2D Gnuplot matrix format.
+         * \param[in] filename the name of the file to write to. */
+        inline void output_source(const char* filename) {
+            gnuplot_output(filename,f,n,n,-1+h,1-h,-1+h,1-h);
+        }
     private:
-        double lambda(int j);
+        /** An array holding the eigenvalues of the one-dimensional Poisson
+         * matrix T_N. */
+        double* const lam;
+        /** The FFTW plan for converting the source term into the frequency
+         * domain. */
         fftw_plan plan_fwd;
+        /** The FFTW plan for converting the frequency domain back to the
+         * solution. */
         fftw_plan plan_bck;
 };
 
