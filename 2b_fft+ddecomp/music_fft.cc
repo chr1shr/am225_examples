b9e17b2b864a876a111b7099e4895a8678df6715
Chris Rycroft <chr@seas.harvard.edu>
2018-02-25 09:50:58 -0500

    Add FFTW timing program.

--- 2b_fft+ddecomp/music_fft.cc
+++ 2b_fft+ddecomp/music_fft.cc
@@ -3,20 +3,20 @@
 #include <fftw3.h>
 #include "omp.h"
 
-const int n=8192;
+const int n=4096;
 
 int main() {
 
     // Read in the binary data in single precision
-    float e[n];
-    FILE *fp=fopen("bob.raw","rb");
-    fread(e,sizeof(float),n,fp);
+    float e[2*n];
+    FILE *fp=fopen("asos.raw","rb");
+    fread(e,sizeof(float),2*n,fp);
     fclose(fp);
 
     // Allocate memory for FFTW input data, and convert sound
-    // sample to double precision
+    // sample to double precision, just getting left channel
     double *f=fftw_alloc_real(n),re,im;
-    for(int i=0;i<n;i++) f[i]=e[i];
+    for(int i=0;i<n;i++) f[i]=e[2*i];
 
     // Allocate memory for complex FFTW output data
     int fftn=n/2+1;
@@ -30,7 +30,7 @@ int main() {
     for(int i=0;i<fftn;i++) {
         re=c[i][0];
         im=c[i][1];
-        printf("%g %g\n",44000./(n/2)*i,sqrt(re*re+im*im));
+        printf("%g %g\n",44000./n*i,sqrt(re*re+im*im));
     }
 
     // Free dynamically allocated memory
